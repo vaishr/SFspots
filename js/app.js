4b6f8e7406d90204a3fd6175f3ba65d462d76907
Vaishnavi Reddy <vaireddy11@gmail.com>
2016-03-30 13:25:26 -0700

    open modals if item has more info

--- js/app.js
+++ js/app.js
@@ -1,10 +1,16 @@
+//google places autocomplete
 var autocomplete;
+
+//mapbox map
 var map;
 
 
+//template for pushing new places into places array
 var templateGeo = function(place, note, type) {
     var lat = place.geometry.location.lat();
     var lng = place.geometry.location.lng();
+    console.log('place', place);
+    console.log('placeTemplate', placeTemplate);
 
     //remove country and zip code from address
     function formatAddress(place) {
@@ -16,7 +22,7 @@ var templateGeo = function(place, note, type) {
         return formatted_add;
     }
 
-
+    //object to be returned from calling this function
     var placeTemplate = {
         "type": "Feature",
         "geometry": {
@@ -34,10 +40,15 @@ var templateGeo = function(place, note, type) {
         }
     }
 
+    //add photos if result from places api includes photos
     var placePhotos = function(place) {
         var photoURLs = [];
+        var numberOfPhotos = 10;
         if (place.photos) {
-            for (var i = 0; i < 10; i++) {
+            if (place.photos.length < 10) {
+                numberOfPhotos = place.photos.length;
+            }
+            for (var i = 0; i < numberOfPhotos; i++) {
                 photoURLs.push(place.photos[i].getUrl({
                     'maxWidth': 800,
                     'maxHeight': 800
@@ -47,10 +58,12 @@ var templateGeo = function(place, note, type) {
         }
     }
 
+    //add open hours if result from places api includes opening hours
     var openHours = function(place) {
-        if (place.opening_hours.weekday_text) {
-            placeTemplate.openHours = place.opening_hours.weekday_text;
+        if (!place.opening_hours) {
+            return;
         }
+        else { placeTemplate.openingHours = place.opening_hours.weekday_text };
     }
 
     placePhotos(place);
@@ -63,6 +76,7 @@ var templateGeo = function(place, note, type) {
 
 
 
+//icons and colors for mapbox markers
 var symbols = {
     "cafe": "#FF9800",
     "restaurant": "#EEFF41",
@@ -81,15 +95,21 @@ L.mapbox.accessToken = "pk.eyJ1IjoidmFpcmVkZHkxMSIsImEiOiJhYjVmNmY2MWQ3MmFiNThkZ
 var app = angular.module("app", ["firebase", "ngMaterial"]);
 
 app.controller("AppCtrl", ["$scope", "$mdDialog", "$mdMedia", function($scope, $mdDialog, $mdMedia) {
+    //storage for added places
     $scope.places = [];
+    //storage for added markers
     $scope.markers = [];
+    //$scope.place refers to text entered in add place field
     $scope.place = "";
     $scope.init = function() {
         var input = document.getElementById('placeInput');
+        //set autocomplete in place input field
         autocomplete = new google.maps.places.Autocomplete(input);
+        //add mapbox map
         map = L.mapbox.map("map", "mapbox.wheatpaste", {
             attributionControl: false
         }).addControl(L.mapbox.shareControl());;
+        //set view to san francisco 
         map.setView([37.763, -122.482], 13);
         var myLayer = L.mapbox.featureLayer().addTo(map);
     }
@@ -97,14 +117,22 @@ app.controller("AppCtrl", ["$scope", "$mdDialog", "$mdMedia", function($scope, $
     $scope.placeType = "star";
     google.maps.event.addDomListener(window, "load", $scope.init);
 
+    //set map view to san francisco 
     $scope.resetMap = function() {
         map.setView([37.763, -122.482], 13);
     }
     $scope.filter = function() {
 
     }
-    $scope.addMarker = function(loc, note, type) {
+    $scope.hasAdditionalInfo = function(placeIndex) {
+        var place = $scope.places[placeIndex];
+        if (place.properties.phone_number || place.properties.website || place.photos) {
+            return true;
+        }
+    return false;
+    }
 
+    $scope.addMarker = function(loc, note, type) {
         var newMarker = L.marker([loc.geometry.coordinates[0], loc.geometry.coordinates[1]], {
             icon: L.mapbox.marker.icon({
                 'marker-color': symbols[type],
@@ -129,10 +157,6 @@ app.controller("AppCtrl", ["$scope", "$mdDialog", "$mdMedia", function($scope, $
         if (note) {
             content += "<h4><i>" + note + "</i></h4>";
         }
-        if (loc.properties.phone_number) {
-            content += "<br><p>" + loc.properties.phone_number + "</p>";
-
-        }
         $scope.markers.push(newMarker);
         console.log("$scope.markers", $scope.markers);
         newMarker.addTo(map);
@@ -141,6 +165,8 @@ app.controller("AppCtrl", ["$scope", "$mdDialog", "$mdMedia", function($scope, $
 
     $scope.markerAnimate;
     $scope.markerUnanimate;
+
+    //animate leaflet marker on map on mouseover of corresponding place in list, using Leaflet smooth marker bouncing plugin
     $scope.animateMarker = function(placeIndex) {
         console.log("calling animateMarker");
         var markerID = $scope.places[placeIndex].properties.place_id;
@@ -151,6 +177,8 @@ app.controller("AppCtrl", ["$scope", "$mdDialog", "$mdMedia", function($scope, $
         }
         $scope.markers[$scope.markerAnimate].bounce(3);
     };
+
+    //stop marker animation on mouseout
     $scope.stopAnimate = function(placeIndex) {
         var markerID = $scope.places[placeIndex].properties.place_id;
         for (var i = 0; i < $scope.markers.length; i++) {
@@ -161,6 +189,7 @@ app.controller("AppCtrl", ["$scope", "$mdDialog", "$mdMedia", function($scope, $
         }
     }
 
+    //called upon submitting a new place, pushes applies template function to place and then adds it to $scope.places 
     $scope.submit = function() {
         var note = $scope.placeNote;
         var type = $scope.placeType;
@@ -180,8 +209,6 @@ app.controller("AppCtrl", ["$scope", "$mdDialog", "$mdMedia", function($scope, $
                 $scope.placeType = "star";
                 return true;
             }
-
-
 
             for (var i = 0; i < $scope.places.length; i++) {
                 if (place.place_id === $scope.places[i].properties.place_id) {
@@ -228,20 +255,22 @@ app.controller("AppCtrl", ["$scope", "$mdDialog", "$mdMedia", function($scope, $
     };
 
     $scope.showAlert = function(ev, placeIndex) {
-        var useFullScreen = ($mdMedia('sm') || $mdMedia('xs')) && $scope.customFullscreen;
-        $mdDialog.show({
-            controller: "DialogCtrl",
-            templateUrl: 'modal.html',
-            parent: angular.element(document.body),
-            targetEvent: ev,
-            controllerAs: 'ctrl',
-            bindToController: true,
-            clickOutsideToClose: true,
-            fullscreen: useFullScreen,
-            locals: {
-                place: $scope.places[placeIndex]
-            }
-        });
+        if ($scope.hasAdditionalInfo(placeIndex)) {
+            var useFullScreen = ($mdMedia('sm') || $mdMedia('xs')) && $scope.customFullscreen;
+            $mdDialog.show({
+                controller: "DialogCtrl",
+                templateUrl: 'modal.html',
+                parent: angular.element(document.body),
+                targetEvent: ev,
+                controllerAs: 'ctrl',
+                bindToController: true,
+                clickOutsideToClose: true,
+                fullscreen: useFullScreen,
+                locals: {
+                    place: $scope.places[placeIndex]
+                }
+            });
+        }
 
         $scope.$watch(function() {
             return $mdMedia('xs') || $mdMedia('sm');
